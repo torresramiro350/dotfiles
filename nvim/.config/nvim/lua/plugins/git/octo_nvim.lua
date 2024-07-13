f675e62d80e0b927c5c87201fd4486341b69f686
Ramiro Torres-Escobedo <rtorrese@proton.me>
2024-07-14 04:44:15 +0800

    adding some extra keybindings for the lspconfig of neovim

--- nvim/.config/nvim/lua/plugins/git/octo_nvim.lua
+++ nvim/.config/nvim/lua/plugins/git/octo_nvim.lua
@@ -1,38 +1,38 @@
 require("groups.utility_funcs")
 return {
-	-- priority = 1000,
-	"pwntester/octo.nvim",
-	-- event = "VeryLazy",
-	-- event = { "BufRead", "BufReadPost" },
-	event = { "BufReadPre", "BufReadPost" },
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope.nvim",
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		local octo = require("octo")
-		octo.setup({
-			picker = "telescope",
-			picker_config = {
-				checkout_pr = { lhs = "<C-o>", desc = "Checkout pull request" },
-				merge_pr = { lhs = "<C-r>", desc = "Merge pull request" },
-			},
-		})
-		-- PR
-		nmap("n", "<leader>pa", "<cmd>Octo pr create<cr>", { desc = "Create PR" })
-		nmap("n", "<leader>pl", "<cmd>Octo pr list<cr>", { desc = "PRs list" })
-		nmap("n", "<leader>po", "<cmd>Octo pr checkout<cr>", { desc = "Checkout PR" })
-		nmap("n", "<leader>pd", "<cmd>Octo pr diff<cr>", { desc = "Show PR diff" })
-		nmap("n", "<leader>pc", "<cmd>Octo pr commits<cr>", { desc = "List PR commits" })
-		nmap("n", "<leader>pf", "<cmd>Octo pr changes<cr>", { desc = "List PR files" })
-		nmap("n", "<leader>pm", "<cmd>Octo pr merge<cr>", { desc = "Merge PR" })
-		-- Issues
-		nmap("n", "<leader>il", "<cmd>Octo issue list<cr>", { desc = "List issues" })
-		nmap("n", "<leader>io", "<cmd>Octo issue create<cr>", { desc = "Create issue" })
-		nmap("n", "<leader>ic", "<cmd>Octo issue close<cr>", { desc = "Close issue" })
-		nmap("n", "<leader>ib", "<cmd>Octo issue browser<cr>", { desc = "Open issue in browser" })
-		nmap("n", "<leader>ca", "<cmd>Octo comment add<cr>", { desc = "Add comment" })
-	end,
-	cond = in_git(),
+  -- priority = 1000,
+  "pwntester/octo.nvim",
+  -- event = "VeryLazy",
+  -- event = { "BufRead", "BufReadPost" },
+  event = { "BufReadPre", "BufReadPost" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local octo = require("octo")
+    octo.setup({
+      picker = "telescope",
+      picker_config = {
+        checkout_pr = { lhs = "<C-o>", desc = "Checkout pull request" },
+        merge_pr = { lhs = "<C-r>", desc = "Merge pull request" },
+      },
+    })
+    -- PR
+    nmap("n", "<leader>pa", "<cmd>Octo pr create<cr>", { desc = "Create PR" })
+    nmap("n", "<leader>pl", "<cmd>Octo pr list<cr>", { desc = "PRs list" })
+    nmap("n", "<leader>po", "<cmd>Octo pr checkout<cr>", { desc = "Checkout PR" })
+    nmap("n", "<leader>pd", "<cmd>Octo pr diff<cr>", { desc = "Show PR diff" })
+    nmap("n", "<leader>pc", "<cmd>Octo pr commits<cr>", { desc = "List PR commits" })
+    nmap("n", "<leader>pf", "<cmd>Octo pr changes<cr>", { desc = "List PR files" })
+    nmap("n", "<leader>pm", "<cmd>Octo pr merge<cr>", { desc = "Merge PR" })
+    -- Issues
+    nmap("n", "<leader>il", "<cmd>Octo issue list<cr>", { desc = "List issues" })
+    nmap("n", "<leader>io", "<cmd>Octo issue create<cr>", { desc = "Create issue" })
+    nmap("n", "<leader>ic", "<cmd>Octo issue close<cr>", { desc = "Close issue" })
+    nmap("n", "<leader>ib", "<cmd>Octo issue browser<cr>", { desc = "Open issue in browser" })
+    nmap("n", "<leader>ca", "<cmd>Octo comment add<cr>", { desc = "Add comment" })
+  end,
+  cond = in_git(),
 }
--- nvim/.config/nvim/lua/plugins/lsp/nvim_lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/nvim_lspconfig.lua
@@ -1,7 +1,7 @@
 return {
   -- LSP Configuration & Plugins
   "neovim/nvim-lspconfig",
-  event = { "BufReadPost", "BufNewFile" },
+  event = { "BufReadPre", "BufReadPost", "BufNewFile" },
   dependencies = {
     -- Automatically install LSPs to stdpath for neovim
     {
@@ -121,6 +121,8 @@ return {
 
       nmap("<leader>xl", vim.diagnostic.setloclist, "Open [D]iagnostics [L]ist")
       nmap("<leader>xq", vim.diagnostic.setqflist, "Open [Q]uickfix [L]ist")
+      nmap("<leader>Q", "<cmd>cclose<cr>", "Close quickfix list")
+      nmap("<leader>L", "<cmd>lclose<cr>", "Close location list")
 
       -- Lesser used LSP functionality
       nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
@@ -263,6 +265,36 @@ return {
     lspconfig.lua_ls.setup({
       capabilities = capabilities,
       on_attach = on_attach,
+      -- provide the completion for neovim
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+
+        if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+          return
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = "LuaJIT",
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME,
+              -- Depending on the usage, you might want to add additional paths here.
+              -- "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            },
+            -- or pull in all of 'runtimepath'.
+            -- NOTE: this is a lot slower
+            --
+            -- library = vim.api.nvim_get_runtime_file("", true)
+          },
+        })
+      end,
       settings = {
         Lua = {
           workspace = { checkThirdParty = false },
