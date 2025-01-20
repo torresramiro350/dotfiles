f9c714264c93356ea1b5af5871fb357fa15af422
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-01-20 23:34:47 +0800

    minor cleanup

--- nvim/.config/nvim/lua/plugins/completion/blink_cmp.lua
+++ nvim/.config/nvim/lua/plugins/completion/blink_cmp.lua
@@ -85,18 +85,6 @@ return {
     },
     snippets = {
       preset = "mini_snippets",
-      -- Function to use when expanding LSP provided snippets
-      expand = function(snippet)
-        vim.snippet.expand(snippet)
-      end,
-      -- Function to use when checking if a snippet is active
-      active = function(filter)
-        return vim.snippet.active(filter)
-      end,
-      -- Function to use when jumping between tab stops in a snippet, where direction can be negative or positive
-      jump = function(direction)
-        vim.snippet.jump(direction)
-      end,
     },
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
