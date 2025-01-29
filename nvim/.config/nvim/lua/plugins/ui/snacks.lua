1bb3e574aa9aa7da724af0d7bea5de97a0f35861
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-01-29 21:04:29 +0800

    adding layouts (don't really know what it does, but I'll keep it for now)

--- nvim/.config/nvim/lua/plugins/ui/snacks.lua
+++ nvim/.config/nvim/lua/plugins/ui/snacks.lua
@@ -12,6 +12,9 @@ return {
     statuscolumn = { enabled = true },
     scroll = { enabled = true },
     words = { enabled = true },
+    layout = {
+      opts = {},
+    },
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
