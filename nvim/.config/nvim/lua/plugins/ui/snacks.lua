7e26583d94dbd2e8d19b991b23593a0245cf6724
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-01-31 02:12:27 +0800

    minor change to snacks picker

--- nvim/.config/nvim/lua/plugins/ui/snacks.lua
+++ nvim/.config/nvim/lua/plugins/ui/snacks.lua
@@ -122,7 +122,7 @@ return {
     -- search
     { '<leader>s"', function() Snacks.picker.registers() end,             desc = "Registers" },
     { "<leader>sa", function() Snacks.picker.autocmds() end,              desc = "Autocmds" },
-    -- { "<leader>sc",      function() Snacks.picker.command_history() end,      desc = "Command History" },
+    { "<leader>sc", function() Snacks.picker.command_history() end,       desc = "Command History" },
     { "<leader>sC", function() Snacks.picker.commands() end,              desc = "Commands" },
     { "<leader>sd", function() Snacks.picker.diagnostics() end,           desc = "Diagnostics" },
     { "<leader>sh", function() Snacks.picker.help() end,                  desc = "Help Pages" },
@@ -130,8 +130,8 @@ return {
     { "<leader>sj", function() Snacks.picker.jumps() end,                 desc = "Jumps" },
     { "<leader>sk", function() Snacks.picker.keymaps() end,               desc = "Keymaps" },
     { "<leader>sl", function() Snacks.picker.loclist() end,               desc = "Location List" },
-    { "<leader>sM", function() Snacks.picker.man() end,                   desc = "Man Pages" },
     { "<leader>sm", function() Snacks.picker.marks() end,                 desc = "Marks" },
+    { "<leader>sM", function() Snacks.picker.man() end,                   desc = "Man Pages" },
     { "<leader>sr", function() Snacks.picker.resume() end,                desc = "Resume" },
     { "<leader>sq", function() Snacks.picker.qflist() end,                desc = "Quickfix List" },
     { "<leader>uC", function() Snacks.picker.colorschemes() end,          desc = "Colorschemes" },
