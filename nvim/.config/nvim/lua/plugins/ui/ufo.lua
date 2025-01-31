2d0e04d78f148d9c299e5e132490bdd25b3185d3
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-02-01 02:41:31 +0800

    minor change to naming of variables to handler function

--- nvim/.config/nvim/lua/plugins/ui/ufo.lua
+++ nvim/.config/nvim/lua/plugins/ui/ufo.lua
@@ -6,32 +6,32 @@ return {
 	enabled = true,
 	config = function()
 		-- add fancy fold text
-		local handler = function(virtual_text, line_number, end_line_number, width, truncate)
-			local new_viruatl_text = {}
-			local suffix = (" 󰁂 %d "):format(end_line_number - line_number)
-			local suf_width = vim.fn.strdisplaywidth(suffix)
-			local target_width = width - suf_width
-			local current_width = 0
-
-			for _, chunk in ipairs(virtual_text) do
-				local chunk_text = chunk[1]
-				local chunk_width = vim.fn.strdisplaywidth(chunk_text)
-				if target_width > current_width + chunk_width then
-					table.insert(new_viruatl_text, chunk)
+		local handler = function(virtText, lnum, endLnum, width, truncate)
+			local newVirtText = {}
+			local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+			local sufWidth = vim.fn.strdisplaywidth(suffix)
+			local targetWidth = width - sufWidth
+			local curWidth = 0
+			for _, chunk in ipairs(virtText) do
+				local chunkText = chunk[1]
+				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+				if targetWidth > curWidth + chunkWidth then
+					table.insert(newVirtText, chunk)
 				else
-					chunk_text = truncate(chunk_text, target_width - current_width)
-					local highlight_group = chunk[2]
-					table.insert(new_viruatl_text, { chunk_text, highlight_group })
-					chunk_width = vim.fn.strdisplaywidth(chunk_text)
-					if current_width + chunk_width < target_width then
-						suffix = suffix .. (" "):rep(target_width - current_width - chunk_width)
+					chunkText = truncate(chunkText, targetWidth - curWidth)
+					local hlGroup = chunk[2]
+					table.insert(newVirtText, { chunkText, hlGroup })
+					chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					-- str width returned from truncate() may less than 2nd argument, need padding
+					if curWidth + chunkWidth < targetWidth then
+						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
 					end
 					break
 				end
-				current_width = current_width + chunk_width
+				curWidth = curWidth + chunkWidth
 			end
-			table.insert(new_viruatl_text, { suffix, "MoreMsg" })
-			return new_viruatl_text
+			table.insert(newVirtText, { suffix, "MoreMsg" })
+			return newVirtText
 		end
 		local ufo = require("ufo")
 
