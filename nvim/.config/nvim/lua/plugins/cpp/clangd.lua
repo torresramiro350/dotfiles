aefdc42ef3df13525dae29ff474f8fee586fd241
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-02-02 18:44:42 +0800

    adding key to clangd config

--- nvim/.config/nvim/lua/plugins/cpp/clangd.lua
+++ nvim/.config/nvim/lua/plugins/cpp/clangd.lua
@@ -1,27 +1,30 @@
 return {
-  "p00f/clangd_extensions.nvim",
-  lazy = true,
-  config = function() end,
-  opts = {
-    ast = {
-      --These require codicons (https://github.com/microsoft/vscode-codicons)
-      role_icons = {
-        type = "",
-        declaration = "",
-        expression = "",
-        specifier = "",
-        statement = "",
-        ["template argument"] = "",
-      },
-      kind_icons = {
-        Compound = "",
-        Recovery = "",
-        TranslationUnit = "",
-        PackExpansion = "",
-        TemplateTypeParm = "",
-        TemplateTemplateParm = "",
-        TemplateParamObject = "",
-      },
-    },
-  },
+	"p00f/clangd_extensions.nvim",
+	lazy = true,
+	config = function() end,
+	keys = {
+		{ "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+	},
+	opts = {
+		ast = {
+			--These require codicons (https://github.com/microsoft/vscode-codicons)
+			role_icons = {
+				type = "",
+				declaration = "",
+				expression = "",
+				specifier = "",
+				statement = "",
+				["template argument"] = "",
+			},
+			kind_icons = {
+				Compound = "",
+				Recovery = "",
+				TranslationUnit = "",
+				PackExpansion = "",
+				TemplateTypeParm = "",
+				TemplateTemplateParm = "",
+				TemplateParamObject = "",
+			},
+		},
+	},
 }
