064452b75fc7999063187f5ac500262c220de8fc
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-02-04 18:15:34 +0800

    minor organization change to the config file

--- nvim/.config/nvim/lua/plugins/treesitter/tree_sitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter/tree_sitter.lua
@@ -1,126 +1,127 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPre", "BufReadPost" },
-  init = function(plugin)
-    require("lazy.core.loader").add_to_rtp(plugin)
-    require("nvim-treesitter.query_predicates")
-  end,
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = {
-        "bash",
-        "c",
-        "cpp",
-        "cmake",
-        "go",
-        "json",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "ninja",
-        "rst",
-        "python",
-        "printf",
-        "rust",
-        "regex",
-        "toml",
-        "vim",
-        "vimdoc",
-        "yaml",
-      },
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPre", "BufReadPost" },
+	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+	init = function(plugin)
+		require("lazy.core.loader").add_to_rtp(plugin)
+		require("nvim-treesitter.query_predicates")
+	end,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- Add languages to be installed here that you want installed for treesitter
+			ensure_installed = {
+				"bash",
+				"c",
+				"cpp",
+				"cmake",
+				"go",
+				"json",
+				"lua",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"ninja",
+				"rst",
+				"python",
+				"printf",
+				"rust",
+				"regex",
+				"toml",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
 
-      -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-      auto_install = false,
-      highlight = { enable = true, additional_vim_regex_highlighting = true },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
-        },
-      },
-      textobjects = {
-        lsp_interop = {
-          enable = true,
-          border = "rounded",
-          peek_definition_code = {
-            ["<leader>df"] = "@function.outer",
-            ["<leader>dF"] = "@class.outer",
-          },
-        },
-        select = {
-          enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["iC"] = "@class.inner",
-            ["ic"] = "@comment.inner",
-            ["oc"] = "@comment.outer",
-            ["aF"] = "@custom.capture",
-          },
-        },
-        selection_modes = {
-          ["@parameter.outer"] = "v", -- charwise
-          ["@function.outer"] = "V", -- linewise
-          ["@class.outer"] = "<c-v>", -- blockwise
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]f"] = "@function.outer",
-            ["]c"] = "@class.outer",
-            ["]a"] = "@parameter.inner",
-            ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-          },
-          goto_next_end = {
-            ["]F"] = "@function.outer",
-            ["]C"] = "@class.outer",
-            ["]A"] = "@parameter.outer",
-          },
-          goto_previous_start = {
-            ["[f"] = "@function.outer",
-            ["[c"] = "@class.outer",
-            ["[a"] = "@parameter.inner",
-          },
-          goto_previous_end = {
-            ["[F"] = "@function.outer",
-            ["[C"] = "@class.outer",
-            ["[A"] = "@parameter.inner",
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<leader>a"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<leader>A"] = "@parameter.inner",
-          },
-        },
-      },
-    })
-  end,
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      opts = {
-        enable_cmd = false,
-      },
-    },
-  },
+			-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+			auto_install = false,
+			highlight = { enable = true, additional_vim_regex_highlighting = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "gnn",
+					node_incremental = "grn",
+					scope_incremental = "grc",
+					node_decremental = "grm",
+				},
+			},
+			textobjects = {
+				lsp_interop = {
+					enable = true,
+					border = "rounded",
+					peek_definition_code = {
+						["<leader>df"] = "@function.outer",
+						["<leader>dF"] = "@class.outer",
+					},
+				},
+				select = {
+					enable = true,
+					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["iC"] = "@class.inner",
+						["ic"] = "@comment.inner",
+						["oc"] = "@comment.outer",
+						["aF"] = "@custom.capture",
+					},
+				},
+				selection_modes = {
+					["@parameter.outer"] = "v", -- charwise
+					["@function.outer"] = "V", -- linewise
+					["@class.outer"] = "<c-v>", -- blockwise
+				},
+				move = {
+					enable = true,
+					set_jumps = true, -- whether to set jumps in the jumplist
+					goto_next_start = {
+						["]f"] = "@function.outer",
+						["]c"] = "@class.outer",
+						["]a"] = "@parameter.inner",
+						["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+						["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+					},
+					goto_next_end = {
+						["]F"] = "@function.outer",
+						["]C"] = "@class.outer",
+						["]A"] = "@parameter.outer",
+					},
+					goto_previous_start = {
+						["[f"] = "@function.outer",
+						["[c"] = "@class.outer",
+						["[a"] = "@parameter.inner",
+					},
+					goto_previous_end = {
+						["[F"] = "@function.outer",
+						["[C"] = "@class.outer",
+						["[A"] = "@parameter.inner",
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>a"] = "@parameter.inner",
+					},
+					swap_previous = {
+						["<leader>A"] = "@parameter.inner",
+					},
+				},
+			},
+		})
+	end,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		{
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			opts = {
+				enable_cmd = false,
+			},
+		},
+	},
 }
