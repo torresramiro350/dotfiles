0e49d303544e2b2f14aad69fcb8a11c547ec02fd
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-02-05 16:39:29 +0800

    minor cleanup

--- nvim/.config/nvim/lua/plugins/ui/snacks.lua
+++ nvim/.config/nvim/lua/plugins/ui/snacks.lua
@@ -208,8 +208,6 @@ return {
 		-- stylua: ignore end
 	},
 	init = function()
-		-- taken from snack.nvim github
-		---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
 		local progress = vim.defaulttable()
 		vim.api.nvim_create_autocmd("LspProgress", {
 			---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
