9a95a8f58e9a0f29ced15049b34d6ea76f334fef
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-02-08 19:17:26 +0800

    updating the configuration for treesitter-context

--- nvim/.config/nvim/lua/plugins/treesitter/treesitter_context.lua
+++ nvim/.config/nvim/lua/plugins/treesitter/treesitter_context.lua
@@ -1,12 +1,19 @@
 return {
-  "nvim-treesitter/nvim-treesitter-context",
-  config = function()
-    require("treesitter-context").setup({
-      enable = true,
-      max_lines = 2,
-      multiline_threshold = 3,
-    })
-  end,
-  -- event = "VeryLazy",
-  event = { "BufRead", "BufReadPost" },
+	"nvim-treesitter/nvim-treesitter-context",
+	event = "VeryLazy",
+	opts = function()
+		local tsc = require("treesitter-context")
+		Snacks.toggle({
+			name = "Treesitter context",
+			get = tsc.enabled,
+			set = function(state)
+				if state then
+					tsc.enable()
+				else
+					tsc.disable()
+				end
+			end,
+		}):map("<leader>ut")
+		return { mode = "cursor", max_lines = 3, multiline_threshold = 3 }
+	end,
 }
