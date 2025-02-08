824282ed557cb8fe37289c5093ff5763a23bd819
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-02-08 19:49:53 +0800

    adding more keybindings for snacks picker

--- nvim/.config/nvim/lua/plugins/ui/snacks.lua
+++ nvim/.config/nvim/lua/plugins/ui/snacks.lua
@@ -103,29 +103,18 @@ return {
     },
     { "<leader>fr", function() Snacks.picker.recent() end,                desc = "Recent" },
     -- Grep
-    {
-      "<leader>sb",
-      function()
-        Snacks.picker.lines(
-          {
-            layout = {
-              preview = "main",
-              -- preset = "ivy",
-              preset = "vertical",
-            },
-          }
-        )
-      end,
-      desc = "Buffer Lines"
-    },
-    -- { "<leader>fe", function() Snacks.explorer() end,                     desc = "Explorer snacks (current dir)" },
-    -- { "<leader>fe", function() Snacks.explorer() end,                           desc = "Explorer snacks (current directory)" },
+    { "<leader>sb", function() Snacks.picker.lines() end,                 desc = "Buffer Lines" },
     { "<leader>sB", function() Snacks.picker.grep_buffers() end,          desc = "Grep Open Buffers" },
     { "<leader>sg", function() Snacks.picker.grep() end,                  desc = "Grep" },
     { "<leader>sw", function() Snacks.picker.grep_word() end,             desc = "Visual selection or word",    mode = { "n", "x" } },
-    -- git
-    { "<leader>gc", function() Snacks.picker.git_log() end,               desc = "Git Log" },
-    { "<leader>gs", function() Snacks.picker.git_status() end,            desc = "Git Status" },
+    -- Git
+    { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+    { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+    { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+    { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+    { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
+    { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
+    { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
     -- search
     { '<leader>s"', function() Snacks.picker.registers() end,             desc = "Registers" },
     { "<leader>sa", function() Snacks.picker.autocmds() end,              desc = "Autocmds" },
