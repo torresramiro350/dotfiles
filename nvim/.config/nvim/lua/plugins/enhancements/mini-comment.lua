5d9e26367a1d1008bdd5116778290efa2de3803f
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-02-08 19:37:11 +0800

    moving some things around

--- nvim/.config/nvim/lua/plugins/enhancements/mini-comment.lua
+++ nvim/.config/nvim/lua/plugins/enhancements/mini-comment.lua
@@ -1,23 +1,32 @@
 return {
-  "echasnovski/mini.comment",
-  version = false,
-  event = "BufReadPost",
-  config = function()
-    local minicomment = require("mini.comment")
-    local ts_context = require("ts_context_commentstring.internal")
-    -- mini.comment
-    minicomment.setup({
-      options = {
-        custom_commentstring = function()
-          return ts_context.calculate_commentstring() or vim.bo.commentstring
-        end,
-      },
-      mappings = {
-        comment = "gc",
-        comment_line = "gcc",
-        comment_visual = "gc",
-        textobject = "gc",
-      },
-    })
-  end,
+	{
+		"echasnovski/mini.comment",
+		version = false,
+		event = "BufReadPost",
+		config = function()
+			local minicomment = require("mini.comment")
+			local ts_context = require("ts_context_commentstring.internal")
+			-- mini.comment
+			minicomment.setup({
+				options = {
+					custom_commentstring = function()
+						return ts_context.calculate_commentstring() or vim.bo.commentstring
+					end,
+				},
+				mappings = {
+					comment = "gc",
+					comment_line = "gcc",
+					comment_visual = "gc",
+					textobject = "gc",
+				},
+			})
+		end,
+	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = true,
+		opts = {
+			enable_cmd = false,
+		},
+	},
 }
