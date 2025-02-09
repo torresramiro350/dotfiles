2d6f52382cb9116c9cb4c90c3cbc32785cc71447
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-02-09 20:39:24 +0800

    minor change to noice nvim and adding a few keymaps

--- nvim/.config/nvim/lua/plugins/ui/noice.lua
+++ nvim/.config/nvim/lua/plugins/ui/noice.lua
@@ -22,9 +22,9 @@ return {
 	opts = {
 		lsp = {
 			override = {
-				["cmp.entry.get_documentation"] = true,
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
 			},
 		},
 		routes = {
@@ -44,37 +44,24 @@ return {
 			bottom_search = true, -- use a classic bottom cmdline for search
 			command_palette = false, -- position the cmdline and popupmenu together
 			long_message_to_split = true, -- long messages will be sent to a split
-			-- inc_rename = false,        -- enables an input dialog for inc-rename.nvim
-			-- lsp_doc_border = false,    -- add a border to hover docs and signature help
-			-- long_message_split = false,
+			inc_rename = false, -- enables an input dialog for inc-rename.nvim
+			lsp_doc_border = false, -- add a border to hover docs and signature help
 		},
 	},
+	keys = {
   -- stylua: ignore start
-  keys = {
+    { "<leader>sn", "", desc = "+noice"},
+    { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
     { "<leader>snl", function() require("noice").cmd("last") end,    desc = "Show last message in popup", },
     { "<leader>snh", function() require("noice").cmd("history") end, desc = "Show history of messages in popup" },
     { "<leader>snt", function() require("noice").cmd("pick") end,    desc = "Noice picker (Telescope/fzf lua)" },
     { "<leader>sna", function() require("noice").cmd("all") end,     desc = "Noice all" },
     { "<leader>snd", function() require("noice").cmd("dismis") end,  desc = "Noice dismiss" },
-    {
-      "<S-Enter>",
-      function() require("noice").redirective(vim.fn.getcmdline()) end,
-      desc = "Redirect cmdline",
-      mode = "c"
-    },
-    {
-      "<c-f>",
-      function()
-        if not require("noice.lsp").scroll(4) then return "<c-f>" end
-      end,
-      desc = "Scroll forward",
-      silent = true,
-      expr = true,
-      mode = { "n", "i", "s" }
-    },
-    { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, desc = "Scroll backward", silent = true, expr = true, mode = { "n", "i", "s" } },
-  },
-	-- stylua: ignore end
+    { "<leader>snt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
+    { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
+    { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
+		-- stylua: ignore end
+	},
 	config = function(_, opts)
 		if vim.o.filetype == "lazy" then
 			vim.cmd([[messages clear]])
