9a5255638aaddfe304951bc20cc2f0f7132bf4e0
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-02-11 00:03:34 +0800

    cleaning the wezterm configuration

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,46 +4,24 @@ local mux = wezterm.mux
 local config = wezterm.config_builder()
 
 function scheme_for_appearance(appearance)
-  if appearance:find("Dark") then
-    return "Catppuccin Mocha"
-  else
-    return "Catppuccin Latte"
-  end
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
 end
-
--- config.term = "xterm-kitty"
--- config.term = "wezterm"
--- declare fonts for terminal to use
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
 config.font_size = 10.0
 config.line_height = 1.2
-config.font = wezterm.font({ family = "JetBrains Mono", weight = "Medium" })
--- config.font = wezterm.font({ family = "JetBrainsMono Nerd Font", weight = "Medium" })
-
-config.ssh_domains = ssh_domains
-
--- controls the window padding
--- config.window_frame = {
---   font = wezterm.font({ family = "Inter", weight = "Medium" }),
---   font_size = 11,
---   active_titlebar_bg = "#11111B",
---   inactive_titlebar_bg = "#11111B",
---   button_bg = "#11111B",
--- }
+-- config.font = wezterm.font({ family = "JetBrains Mono", weight = "Medium" })
+config.font = wezterm.font({ family = "JetBrainsMono Nerd Font", weight = "Medium" })
 config.window_decorations = "TITLE | RESIZE"
--- config.window_decorations = "RESIZE"
-config.window_padding = {
-  left = 3,
-  right = 3,
-  top = 0,   -- wayland
-  bottom = 0, -- wayland
-}
 config.check_for_updates = false
 config.use_ime = true
 config.enable_scroll_bar = false
 config.enable_tab_bar = false
-config.enable_wayland = false
+config.enable_wayland = true
 
 config.hide_tab_bar_if_only_one_tab = true
 
@@ -61,11 +39,11 @@ config.status_update_interval = 1000
 config.tab_bar_at_bottom = false
 
 config.mouse_bindings = {
-  {
-    event = { Up = { streak = 1, button = "Left" } },
-    mods = "CTRL",
-    action = wezterm.action.OpenLinkAtMouseCursor,
-  },
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
 }
 
 return config
