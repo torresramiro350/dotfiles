91e68766a9d26eb1c9e207d0167b9903167ec32a
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-02-22 10:54:55 +0800

    updating the configuration for neogen

--- nvim/.config/nvim/lua/plugins/documentation/neogen_lua.lua
+++ nvim/.config/nvim/lua/plugins/documentation/neogen_lua.lua
@@ -1,53 +1,53 @@
 require("groups.utility_funcs")
 return {
-  "danymat/neogen",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  event = { "BufNewFile ", "BufReadPre" },
-  cmd = "Neogen",
-  keys = {
-    {
-      "<leader>cn",
-      function()
-        require("neogen").generate()
-      end,
-      desc = "Generate annotations (Neogen)",
-    },
-  },
-  config = function()
-    local neogen = require("neogen")
-    neogen.setup({
-      -- snippet_engine = "luasnip",
-      snippet_engine = "mini",
-      input_after_comment = true,
-      languages = {
-        lua = {
-          template = {
-            -- annotation_convention = "emmylua",
-            annotation_convention = "ldoc",
-          },
-        },
-        python = {
-          template = {
-            annotation_convention = "reST",
-            -- annotation_convention = "numpydoc",
-          },
-        },
-        cpp = {
-          template = {
-            annotation_convention = "doxygen",
-          },
-        },
-        rust = {
-          template = {
-            annotation_convention = "rustdoc",
-          },
-        },
-        sh = {
-          template = {
-            annotation_convention = "google_bash",
-          },
-        },
-      },
-    })
-  end,
+	"danymat/neogen",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	event = { "BufNewFile ", "BufReadPre" },
+	cmd = "Neogen",
+	keys = {
+		{
+			"<leader>cn",
+			function()
+				require("neogen").generate()
+			end,
+			desc = "Generate annotations (Neogen)",
+		},
+	},
+	opts = {
+		-- snippet_engine = "luasnip",
+		snippet_engine = "mini",
+		input_after_comment = true,
+		languages = {
+			lua = {
+				template = {
+					annotation_convention = "emmylua",
+				},
+			},
+			python = {
+				template = {
+					annotation_convention = "reST",
+				},
+			},
+			c = {
+				template = {
+					annotation_convention = "doxygen",
+				},
+			},
+			cpp = {
+				template = {
+					annotation_convention = "doxygen",
+				},
+			},
+			rust = {
+				template = {
+					annotation_convention = "rustdoc",
+				},
+			},
+			sh = {
+				template = {
+					annotation_convention = "google_bash",
+				},
+			},
+		},
+	},
 }
