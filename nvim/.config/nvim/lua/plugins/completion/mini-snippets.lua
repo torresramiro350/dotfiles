0c1903439e26a11cef0a6968b8725b93dd9e317f
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-02-22 10:59:31 +0800

    minor formatting change

--- nvim/.config/nvim/lua/plugins/completion/mini-snippets.lua
+++ nvim/.config/nvim/lua/plugins/completion/mini-snippets.lua
@@ -1,31 +1,31 @@
 return {
-  "echasnovski/mini.snippets",
-  event = "InsertEnter",
-  dependencies = "rafamadriz/friendly-snippets",
-  version = false,
-  opts = function()
-    local mini_snippets = require("mini.snippets")
-    local expand_select_override = function(snippets, insert)
+	"echasnovski/mini.snippets",
+	event = "InsertEnter",
+	dependencies = "rafamadriz/friendly-snippets",
+	version = false,
+	opts = function()
+		local mini_snippets = require("mini.snippets")
+		local expand_select_override = function(snippets, insert)
       -- stylua: ignore
       -- if cmp.visible() then cmp.close() end
       -- Schedule, otherwise blink's virtual text is not removed on vim.ui.select
       require('blink.cmp').cancel()
-      vim.schedule(function()
-        MiniSnippets.default_select(snippets, insert)
-        -- MiniSnippets.default_select(snippets, insert)
-      end)
-    end
-    -- return ret
-    return {
-      snippets = { mini_snippets.gen_loader.from_lang() },
-      expand = {
-        select = function(snippets, insert)
-          -- Close completion window on snippet select - vim.ui.select
-          -- Needed to remove virtual text for fzf-lua and telescope, but not for mini.pick...
-          local select = expand_select_override or MiniSnippets.default_select
-          select(snippets, insert)
-        end,
-      },
-    }
-  end,
+			vim.schedule(function()
+				MiniSnippets.default_select(snippets, insert)
+				-- MiniSnippets.default_select(snippets, insert)
+			end)
+		end
+		-- return ret
+		return {
+			snippets = { mini_snippets.gen_loader.from_lang() },
+			expand = {
+				select = function(snippets, insert)
+					-- Close completion window on snippet select - vim.ui.select
+					-- Needed to remove virtual text for fzf-lua and telescope, but not for mini.pick...
+					local select = expand_select_override or MiniSnippets.default_select
+					select(snippets, insert)
+				end,
+			},
+		}
+	end,
 }
