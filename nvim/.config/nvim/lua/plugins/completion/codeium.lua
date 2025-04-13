7f23b6e1546459335b6bd2fd159f2833856beb9b
Ramiro <rtorrese@proton.me>
2025-04-13 14:54:14 +0800

    minor formatting changes

--- nvim/.config/nvim/lua/plugins/completion/codeium.lua
+++ nvim/.config/nvim/lua/plugins/completion/codeium.lua
@@ -1,20 +1,20 @@
 require("groups.utility_funcs")
 return {
-  "Exafunction/codeium.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  enabled = true,
-  event = { "InsertEnter" },
-  build = ":Codeium Auth",
-  cmd = "Codeium",
-  opts = {
-    enable_cmp_source = vim.g.ai_cmp,
-    virtual_text = {
-      enabled = not vim.g.ai_cmp,
-      key_bindings = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
-        next = "<M-]>",
-        prev = "<M-[>",
-      },
-    },
-  },
+	"Exafunction/codeium.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	enabled = true,
+	event = { "InsertEnter" },
+	build = ":Codeium Auth",
+	cmd = "Codeium",
+	opts = {
+		enable_cmp_source = vim.g.ai_cmp,
+		virtual_text = {
+			enabled = not vim.g.ai_cmp,
+			key_bindings = {
+				accept = false, -- handled by nvim-cmp / blink.cmp
+				next = "<M-]>",
+				prev = "<M-[>",
+			},
+		},
+	},
 }
