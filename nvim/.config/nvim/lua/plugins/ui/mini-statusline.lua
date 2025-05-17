f8eac15412ee671cc2bf97cf095d69d9ebcc3fcb
Ramiro <rtorrese@proton.me>
2025-05-17 14:17:30 +0800

    updating config of mini-statusline

--- nvim/.config/nvim/lua/plugins/ui/mini-statusline.lua
+++ nvim/.config/nvim/lua/plugins/ui/mini-statusline.lua
@@ -1,58 +1,61 @@
 return {
-  "echasnovski/mini.statusline",
-  version = false,
-  event = { "VimEnter" },
-  config = function()
-    local statusline = require("mini.statusline")
-    -- Autocmd to track macro recording, And redraw statusline, which trigger
-    -- macro function of mini.statusline
-    vim.api.nvim_create_autocmd("RecordingEnter", {
-      pattern = "*",
-      callback = function()
-        vim.cmd("redrawstatus")
-      end,
-    })
-    -- Autocmd to track the end of macro recording
-    vim.api.nvim_create_autocmd("RecordingLeave", {
-      pattern = "*",
-      callback = function()
-        vim.cmd("redrawstatus")
-      end,
-    })
-    statusline.setup({
-      use_icons = true,
-      set_vim_settings = true,
-      content = {
-        active = function()
-          local check_macro_recording = function()
-            if vim.fn.reg_recording() ~= "" then
-              return "recording @" .. vim.fn.reg_recording()
-            else
-              return ""
-            end
-          end
-          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-          local git = MiniStatusline.section_git({ trunc_width = 40 })
-          local diff = MiniStatusline.section_diff({ trunc_width = 75 })
-          local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-          local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
-          local filename = MiniStatusline.section_filename({ trunc_width = 140 })
-          local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-          local location = MiniStatusline.section_location({ trunc_width = 75 })
-          local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
-          local macro = check_macro_recording()
-          return MiniStatusline.combine_groups({
-            { hl = mode_hl,                 strings = { mode } },
-            { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
-            "%<", -- Mark general truncate point
-            { hl = "MiniStatuslineFilename", strings = { filename } },
-            "%=", -- End left alignment
-            { hl = "MiniStatuslineFilename", strings = { macro } },
-            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
-            { hl = mode_hl,                  strings = { search, location } },
-          })
-        end,
-      },
-    })
-  end,
+	"echasnovski/mini.statusline",
+	version = false,
+	event = { "VimEnter" },
+	opts = function()
+		vim.api.nvim_create_autocmd("RecordingEnter", {
+			pattern = "*",
+			callback = function()
+				vim.cmd("redrawstatus")
+			end,
+		})
+		-- Autocmd to track the end of macro recording
+		vim.api.nvim_create_autocmd("RecordingLeave", {
+			pattern = "*",
+			callback = function()
+				vim.cmd("redrawstatus")
+			end,
+		})
+		return {
+			use_icons = true,
+			set_vim_settings = true,
+			content = {
+				active = function()
+					local check_macro_recording = function()
+						if vim.fn.reg_recording() ~= "" then
+							return "recording @" .. vim.fn.reg_recording()
+						else
+							return ""
+						end
+					end
+					local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+					local git = MiniStatusline.section_git({ trunc_width = 40 })
+					local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+					local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+					local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+					local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+					local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+					local location = MiniStatusline.section_location({ trunc_width = 75 })
+					local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+					local macro = check_macro_recording()
+					return MiniStatusline.combine_groups({
+						{ hl = mode_hl, strings = { mode } },
+						{ hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+						"%<", -- Mark general truncate point
+						{ hl = "MiniStatuslineFilename", strings = { filename } },
+						"%=", -- End left alignment
+						{ hl = "MiniStatuslineFilename", strings = { macro } },
+						{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+						{ hl = mode_hl, strings = { search, location } },
+					})
+				end,
+			},
+		}
+	end,
+	config = function(_, opts)
+		local statusline = require("mini.statusline")
+		-- Autocmd to track macro recording, And redraw statusline, which trigger
+		-- macro function of mini.statusline
+		statusline.setup(opts)
+	end,
 }
