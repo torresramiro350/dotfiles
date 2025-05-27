8f0a2c7f4112863e8851b1efea529c2ef730beaa
Ramiro <rtorrese@proton.me>
2025-05-27 20:45:56 +0800

    minor change (pretty much taking the configuration settings from Lazyvim's example)

--- nvim/.config/nvim/lua/plugins/enhancements/mini-ai.lua
+++ nvim/.config/nvim/lua/plugins/enhancements/mini-ai.lua
@@ -8,30 +8,18 @@ return {
 			n_lines = 500,
 			search_method = "cover_or_next",
 			custom_textobjects = {
-				o = ai.gen_spec.treesitter({
+				o = ai.gen_spec.treesitter({ -- code block
 					a = { "@block.outer", "@conditional.outer", "@loop.outer" },
 					i = { "@block.inner", "@conditional.inner", "@loop.inner" },
 				}),
-				f = ai.gen_spec.treesitter({
-					a = { "@function.outer" },
-					i = { "@function.inner" },
-				}),
-				c = ai.gen_spec.treesitter({
-					a = { "@class.outer" },
-					i = { "@class.inner" },
-				}),
-				t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
-				d = { "%f[%d]%d+" },
-				e = {
-					{
-						"%u[%l%d]+%f[^%l%d]",
-						"%f[%S][%l%d]+%f[^%l%d]",
-						"%f[%P][%l%d]+%f[^%l%d]",
-						"^[%l%d]+%f[^%l%d]",
-					},
+				f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+				c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+				t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+				e = { -- Word with case
+					{ "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
 					"^().*()$",
 				},
-				u = ai.gen_spec.function_call(),
+				u = ai.gen_spec.function_call(), -- u for usage
 				U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }),
 			},
 			mappings = {
