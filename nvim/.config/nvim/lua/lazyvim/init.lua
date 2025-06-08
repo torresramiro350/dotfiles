2e3b7a1b4b40b33bd44bc26194e0750b23f8656a
Ramiro <rtorrese@proton.me>
2025-06-08 21:17:35 +0800

    minor changes to the lazyvim configuration

--- nvim/.config/nvim/lua/lazyvim/init.lua
+++ nvim/.config/nvim/lua/lazyvim/init.lua
@@ -3,30 +3,31 @@
 -- `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-  checker = { enabled = true },
-  -- { import = "plugins" }, -- enough if all plugins live in a single directory
-  { import = "plugins.enhancements" },
-  { import = "plugins.tmux" },
-  { import = "plugins.completion" },
-  { import = "plugins.cpp" },
-  { import = "plugins.documentation" },
-  { import = "plugins.notes" },
-  { import = "plugins.git" },
-  { import = "plugins.lsp" },
-  { import = "plugins.rust" },
-  { import = "plugins.sessions" },
-  { import = "plugins.treesitter" },
-  { import = "plugins.telescope" },
-  { import = "plugins.ui" },
+	checker = { enabled = true },
+	-- { import = "plugins" }, -- enough if all plugins live in a single directory
+	{ import = "plugins.enhancements" },
+	{ import = "plugins.tmux" },
+	{ import = "plugins.completion" },
+	{ import = "plugins.cpp" },
+	{ import = "plugins.documentation" },
+	{ import = "plugins.notes" },
+	{ import = "plugins.git" },
+	{ import = "plugins.lsp" },
+	{ import = "plugins.rust" },
+	{ import = "plugins.sessions" },
+	{ import = "plugins.treesitter" },
+	{ import = "plugins.telescope" },
+	{ import = "plugins.typing" },
+	{ import = "plugins.ui" },
 })
