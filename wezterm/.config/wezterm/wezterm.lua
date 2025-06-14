47b5878bc19f6d233c751bfdf3c8ccc227b38773
Ramiro <rtorrese@proton.me>
2025-06-14 20:27:57 +0800

    small changes to ghostty config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,22 +1,36 @@
 -- Basing myself heavily on others' configuration files
 local wezterm = require("wezterm")
-local mux = wezterm.mux
 local config = wezterm.config_builder()
+local appearance = require("appearance")
 
-function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "Catppuccin Mocha"
-	else
-		return "Catppuccin Latte"
-	end
+if appearance.is_dark() then
+	config.color_scheme = "Catppuccin Mocha"
+else
+	config.color_scheme = "Catppuccin Latte"
 end
-config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
-config.font_size = 10.0
-config.line_height = 1.2
--- config.font = wezterm.font({ family = "JetBrains Mono", weight = "Medium" })
-config.font = wezterm.font({ family = "JetBrainsMono Nerd Font", weight = "Medium" })
+config.window_background_opacity = 1
+config.font_size = 10
+config.line_height = 1.15
+-- config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font("Maple Mono NF", { weight = "Medium", italic = false })
+-- config.font = wezterm.font("JetBrains Mono", { weight = "DemiBold", italic = false })
+-- config.font = wezterm.font("JetBrainsMono NF", { weight = "DemiBold", italic = false })
+
 config.window_decorations = "TITLE | RESIZE"
+-- config.window_decorations = "RESIZE|TITLE|INTEGRATED_BUTTONS"
+config.window_frame = {
+	font = wezterm.font({ family = "Adwaitas Mono", weight = "Bold", italic = false }),
+	font_size = 11,
+}
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
 config.check_for_updates = false
 config.use_ime = true
 config.enable_scroll_bar = false
@@ -30,13 +44,15 @@ config.window_close_confirmation = "NeverPrompt"
 config.exit_behavior = "CloseOnCleanExit"
 
 -- specify the shell to use with wezterm
-config.default_prog = { "/usr/bin/fish" }
--- config.default_prog = {"/usr/bin/nu"}
 config.scrollback_lines = 5000
 config.default_workspace = "main"
 config.use_fancy_tab_bar = true
 config.status_update_interval = 1000
 config.tab_bar_at_bottom = false
+
+config.keys = {
+	{ key = "A", mods = "CTRL|SHIFT", action = wezterm.action.QuickSelect },
+}
 
 config.mouse_bindings = {
 	{
