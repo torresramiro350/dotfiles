6032a6e1ae3bdb7834b6010c1d5162be8b02bc6f
Ramiro <rtorrese@proton.me>
2025-06-19 23:10:51 +0800

    adding some extra configuration for yaml files (wip)

--- nvim/.config/nvim/lua/plugins/lsp/nvim_lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/nvim_lspconfig.lua
@@ -193,6 +193,25 @@ return {
 							},
 						},
 					},
+					on_new_config = function(new_config)
+						new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+							"force",
+							new_config.settings.yaml.schemas or {},
+							require("schemastore").yaml.schemas()
+						)
+					end,
+					settings = {
+						redhat = { telemetry = { enabled = false } },
+						yaml = {
+							keyOrdering = false,
+							format = { enable = true },
+							validate = true,
+							schemaStore = {
+								enable = false,
+								url = "",
+							},
+						},
+					},
 				},
 				-- Julia (not really using it)
 				julials = {
