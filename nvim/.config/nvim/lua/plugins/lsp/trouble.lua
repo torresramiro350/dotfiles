474649798ff2c2b68650ae9de052255ebc6ac1b7
Ramiro <rtorrese@proton.me>
2025-06-21 15:44:17 +0800

    more cleanup

--- nvim/.config/nvim/lua/plugins/lsp/trouble.lua
+++ nvim/.config/nvim/lua/plugins/lsp/trouble.lua
@@ -20,6 +20,7 @@ return {
     { "<leader>cS", "<cmd>Trouble lsp toggle<cr>", desc = "LSP references/definitions/... (Trouble)" },
     { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
     { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+    { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)", },
     {
       "[q",
       function()
