7bf51707f00676c5f51122f2b5fb3e25da8bf526
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-06-24 14:29:10 +0800

    cleaning the configuration of conform.nvim

--- nvim/.config/nvim/lua/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -2,18 +2,18 @@ return {
 	"stevearc/conform.nvim",
 	dependencies = { "mason.nvim" },
 	lazy = true,
-	event = { "BufWritePre" },
+	event = { "BufReadPre" },
 	command = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>cF",
-			function()
-				require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
-			end,
-			mode = { "n", "v" },
-			desc = "Format Injected Langs",
-		},
-	},
+	-- keys = {
+	-- 	{
+	-- 		"<leader>cF",
+	-- 		function()
+	-- 			require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+	-- 		end,
+	-- 		mode = { "n", "v" },
+	-- 		desc = "Format Injected Langs",
+	-- 	},
+	-- },
 	opts = function()
 		local opts = {
 			default_format_opts = {
