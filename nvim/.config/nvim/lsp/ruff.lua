ff5da1c8b27f18c126878f3e6c31eada3a4f0e6b
Ramiro <rtorrese@proton.me>
2025-06-26 20:45:40 +0800

    some minor configuration changes

--- nvim/.config/nvim/lsp/ruff.lua
+++ nvim/.config/nvim/lsp/ruff.lua
@@ -7,6 +7,9 @@ return {
 	cmd_env = { RUFF_TRACE = "messages" },
 	init_options = {
 		settings = {
+			lint = { enable = true, preview = true },
+			format = { preview = true },
+			configurationPreference = "filesystemFirst",
 			organizeImport = true,
 			lineLength = 88,
 			logLevel = "error",
