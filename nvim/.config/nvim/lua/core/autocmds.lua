4f39d92d7c09eb6d7950527b001c29cb0abd4f9b
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-06-26 11:39:10 +0800

    everything should now be in order (will keep the lspconfig disabled for now), but everything seems to be working

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -1,7 +1,23 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
-autocmd("lspattach", {
-	group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+-- autocmd("lspattach", {
+-- 	group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+-- 	callback = function(args)
+-- 		local client = vim.lsp.get_client_by_id(args.data.client_id)
+-- 		if client == nil then
+-- 			return
+-- 		end
+-- 		if client.name == "ruff" then
+-- 			-- Disable hover in favor of pyright/basedpyright
+-- 			client.server_capabilities.hoverProvider = false
+-- 		end
+-- 	end,
+-- 	desc = "LSP: Disable hover capability from Ruff",
+-- })
+
+-- LSP
+local completion = vim.g.completion_mode or "blink" -- or 'native'
+vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if client == nil then
@@ -11,6 +27,16 @@ autocmd("lspattach", {
 			-- Disable hover in favor of pyright/basedpyright
 			client.server_capabilities.hoverProvider = false
 		end
+		if client then
+			-- Built-in completion
+			if completion == "native" and client:supports_method("textDocument/completion") then
+				vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+			end
+
+			-- Inlay hints
+			if client:supports_method("textDocument/inlayHints") then
+				vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+			end
+		end
 	end,
-	desc = "LSP: Disable hover capability from Ruff",
 })
