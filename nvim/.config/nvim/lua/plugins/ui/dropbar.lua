26c81bd020c8d2dfac273334b38d5dd7bf4279ce
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-06-27 08:27:26 +0800

    cleaning up the configuration for dropbar

--- nvim/.config/nvim/lua/plugins/ui/dropbar.lua
+++ nvim/.config/nvim/lua/plugins/ui/dropbar.lua
@@ -1,15 +1,8 @@
 return {
 	"Bekaboo/dropbar.nvim",
 	event = { "BufRead", "BufReadPre", "BufNewFile" },
-	-- optional, but required for fuzzy finder support
-	dependencies = {
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
-	},
-	config = function()
+	opts = {},
+	config = function(_, opts)
 		local dropbar_api = require("dropbar.api")
-		-- vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
-		-- vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
-		-- vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
 	end,
 }
