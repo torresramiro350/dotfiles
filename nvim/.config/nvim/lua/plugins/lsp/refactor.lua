51df0596514f9af5ac2f033736fac37d7bf7e969
Ramiro <rtorrese@proton.me>
2025-06-28 13:31:38 +0800

    minor adjustment to config and ensure lazy loading for the refactrong plugin

--- nvim/.config/nvim/lua/plugins/lsp/refactor.lua
+++ nvim/.config/nvim/lua/plugins/lsp/refactor.lua
@@ -1,10 +1,38 @@
 return {
 	"ThePrimeagen/refactoring.nvim",
+	lazy = true,
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
 	},
+	opts = {
+		prompt_func_return_type = {
+			go = false,
+			java = false,
+			cpp = false,
+			c = false,
+			h = false,
+			hpp = false,
+			cxx = false,
+		},
+		prompt_func_param_type = {
+			go = false,
+			java = false,
+			cpp = false,
+			c = false,
+			h = false,
+			hpp = false,
+			cxx = false,
+		},
+		printf_statements = {},
+		print_var_statements = {},
+		show_success_message = true, -- shows a message with information about the refactor on success
+		-- i.e. [Refactor] Inlined 3 variable occurrences
+	},
+	config = function(_, opts)
+		require("refactoring").setup(opts)
+	end,
 	keys = {
 		{ "<leader>r", "", desc = "+refactor", mode = { "n", "v" } },
 		{
@@ -89,32 +117,4 @@ return {
 			desc = "Debug Print Variable",
 		},
 	},
-	lazy = false,
-	opts = {
-		prompt_func_return_type = {
-			go = false,
-			java = false,
-			cpp = false,
-			c = false,
-			h = false,
-			hpp = false,
-			cxx = false,
-		},
-		prompt_func_param_type = {
-			go = false,
-			java = false,
-			cpp = false,
-			c = false,
-			h = false,
-			hpp = false,
-			cxx = false,
-		},
-		printf_statements = {},
-		print_var_statements = {},
-		show_success_message = true, -- shows a message with information about the refactor on success
-		-- i.e. [Refactor] Inlined 3 variable occurrences
-	},
-	config = function(_, opts)
-		require("refactoring").setup(opts)
-	end,
 }
