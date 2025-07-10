bc5238cfd5e8d5bf6398d26dd1b8e14a01dbd7aa
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-07-10 14:26:33 +0800

    moving some things around

--- nvim/.config/nvim/lua/plugins/treesitter/tree_sitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter/tree_sitter.lua
@@ -62,6 +62,7 @@ return {
 				"vimdoc",
 				"yaml",
 			},
+			auto_install = true,
 			incremental_selection = {
 				enable = true,
 				keymaps = {
@@ -72,8 +73,38 @@ return {
 				},
 			},
 			textobjects = {
+				select = {
+					enable = false,
+					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["iC"] = "@class.inner",
+						["ic"] = "@comment.inner",
+						["oc"] = "@comment.outer",
+						["aF"] = "@custom.capture",
+					},
+				},
+				selection_modes = {
+					["@parameter.outer"] = "v", -- charwise
+					["@function.outer"] = "V", -- linewise
+					["@class.outer"] = "<c-v>", -- blockwise
+				},
+				lsp_interop = {
+					enable = true,
+					border = "rounded",
+					peek_definition_code = {
+						["<leader>df"] = "@function.outer",
+						["<leader>dF"] = "@class.outer",
+					},
+				},
 				move = {
 					enable = true,
+					set_jumps = true, -- whether to set jumps in the jumplist
 					goto_next_start = {
 						["]f"] = "@function.outer",
 						["]c"] = "@class.outer",
