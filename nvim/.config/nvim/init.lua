175dd43fd703104173bd3ba9b4889ee614c8b3e9
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-07-17 11:13:07 +0200

    cleaning the init lua configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,38 +11,8 @@ vim.cmd.colorscheme("catppuccin")
 require("keymaps.mappings")
 -- require("groups.init")
 require("core.autocmds")
-
-local diagnostics = {
-	update_in_insert = false,
-	severity_sort = true,
-	float = { border = "rounded", source = "if_many" },
-	underline = { severity = vim.diagnostic.severity.ERROR },
-	signs = vim.g.have_nerd_font and {
-		text = {
-			[vim.diagnostic.severity.ERROR] = "󰅚 ",
-			[vim.diagnostic.severity.WARN] = "󰀪 ",
-			[vim.diagnostic.severity.INFO] = "󰋽 ",
-			[vim.diagnostic.severity.HINT] = "󰌶 ",
-		},
-	} or {},
-	virtual_text = {
-		prefix = "●",
-		current_line = true,
-		source = "if_many",
-		spacing = 4,
-		format = function(diagnostic)
-			local diagnostic_message = {
-				[vim.diagnostic.severity.ERROR] = diagnostic.message,
-				[vim.diagnostic.severity.WARN] = diagnostic.message,
-				[vim.diagnostic.severity.INFO] = diagnostic.message,
-				[vim.diagnostic.severity.HINT] = diagnostic.message,
-			}
-			return diagnostic_message[diagnostic.severity]
-		end,
-	},
-}
-
-vim.diagnostic.config(diagnostics)
+-- load the diagnostics configuration
+require("lsp.diagnostics")
 
 vim.lsp.enable({
 	"lua_ls",
