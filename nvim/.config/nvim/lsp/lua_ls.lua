16ac59b4cf587ccc01fbb0687014d1d0edcfc0c0
Ramiro <rtorrese@proton.me>
2025-07-27 03:16:22 +0800

    correctly setup the lua_ls server

--- nvim/.config/nvim/lsp/lua_ls.lua
+++ nvim/.config/nvim/lsp/lua_ls.lua
@@ -3,22 +3,33 @@ return {
 	filetypes = { "lua" },
 	on_attach = Lsp.on_attach,
 	cmd = { "lua-language-server" },
-	Lua = {
-		workspace = { checkThirdParty = false },
-		codeLens = { enable = true },
-		completion = {
-			callSnippet = "Replace",
-		},
-		doc = {
-			privateName = { "^_" },
-		},
-		hint = {
-			enable = true,
-			setType = false,
-			paramType = true,
-			paramName = "Disable",
-			semicolon = "Disable",
-			arrayIndex = "Disable",
+	settings = {
+		Lua = {
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME,
+					-- Depending on the usage, you might want to add additional paths
+					-- here.
+					-- '${3rd}/luv/library'
+					-- '${3rd}/busted/library'
+				},
+			},
+			codeLens = { enable = true },
+			completion = {
+				callSnippet = "Replace",
+			},
+			doc = {
+				privateName = { "^_" },
+			},
+			hint = {
+				enable = true,
+				setType = false,
+				paramType = true,
+				paramName = "Disable",
+				semicolon = "Disable",
+				arrayIndex = "Disable",
+			},
 		},
 	},
 }
