f527f54fd1befdee85e5f047d482ab7549e25038
Ramiro <rtorrese@proton.me>
2025-07-28 02:04:54 +0800

    adding more quality of life changes

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,43 +1,93 @@
-local augroup = vim.api.nvim_create_augroup
+-- local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
--- autocmd("lspattach", {
--- 	group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
--- 	callback = function(args)
--- 		local client = vim.lsp.get_client_by_id(args.data.client_id)
--- 		if client == nil then
--- 			return
--- 		end
--- 		if client.name == "ruff" then
--- 			-- Disable hover in favor of pyright/basedpyright
--- 			client.server_capabilities.hoverProvider = false
--- 		end
--- 	end,
--- 	desc = "LSP: Disable hover capability from Ruff",
--- })
+local function augroup(name)
+	return vim.api.nvim_create_augroup("my_nvim_" .. name, { clear = true })
+end
+
+-- resize split if window got resized
+autocmd({ "VimResized" }, {
+	group = augroup("resize_splits"),
+	callback = function()
+		local current_tab = vim.fn.tabpagenr()
+		vim.cmd("tabdo wincmd =")
+		vim.cmd("tabnext " .. current_tab)
+	end,
+})
 
 -- highlight yanked text
-local highlight_group = augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
+autocmd("TextYankPost", {
+	group = augroup("highlight_yank"),
 	callback = function()
-		vim.highlight.on_yank()
+		(vim.hl or vim.highlight).on_yank()
 	end,
-	group = highlight_group,
 	pattern = "*",
 })
 
+-- go to last loc when opening a buffer
+autocmd("BufReadPost", {
+	group = augroup("last_loc"),
+	callback = function(event)
+		local exclude = { "gitcommit" }
+		local buf = event.buf
+		if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+			return
+		end
+		vim.b[buf].lazyvim_last_loc = true
+		local mark = vim.api.nvim_buf_get_mark(buf, '"')
+		local lcount = vim.api.nvim_buf_line_count(buf)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
+
+-- make it easier to close man-files when opened inline
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("man_unlisted"),
+	pattern = { "man" },
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+	end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("wrap_spell"),
+	pattern = { "*.txt", "*.tex", "*.typ", "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = augroup("auto_create_dir"),
+	callback = function(event)
+		if event.match:match("^%w%w+:[\\/][\\/]") then
+			return
+		end
+		-- support both new and old versions of neovim
+		local uv = vim.uv or vim.loop
+		local file = uv.fs_realpath and uv.fs_realpath(event.match) or event.match
+		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+	end,
+})
+
 -- LSP
-local completion = vim.g.completion_mode or "blink" -- or 'native'
+local completion = vim.g.completion_mode or "blink" or "native"
 autocmd("LspAttach", {
-	group = augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+	group = augroup("UserLspConfig"),
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if client == nil then
 			return
 		end
+		-- NOTE: Disable hover in favor of pyright/basedpyright
 		if client.name == "ruff" then
-			-- Disable hover in favor of pyright/basedpyright
 			client.server_capabilities.hoverProvider = false
 		end
+
 		if client then
 			-- Built-in completion
 			if completion == "native" and client:supports_method("textDocument/completion") then
