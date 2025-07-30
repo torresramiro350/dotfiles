a8e7f902e54323d1d4bf84b007cc4ce82e9add9e
Ramiro <rtorrese@proton.me>
2025-07-31 06:05:23 +0800

    updating configuration for neogit

--- nvim/.config/nvim/lua/plugins/git/neogit.lua
+++ nvim/.config/nvim/lua/plugins/git/neogit.lua
@@ -1,20 +1,16 @@
 require("groups.utility_funcs")
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
-
-    -- Only one of these is needed, not both.
-    "nvim-telescope/telescope.nvim", -- optional
-    "ibhagwan/fzf-lua",            -- optional
-  },
-  -- config = true,
-  config = function()
-    local neogit = require("neogit")
-    neogit.setup({})
-  end,
-  -- event = "VeryLazy",
-  event = { "BufReadPre", "BufReadPost" },
-  cond = in_git(),
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
+		"ibhagwan/fzf-lua", -- optional
+	},
+	opts = {},
+	config = function(_, opts)
+		require("neogit").setup(opts)
+	end,
+	-- event = "VeryLazy",
+	event = { "BufReadPre", "BufReadPost" },
+	cond = in_git(),
 }
