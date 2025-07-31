6ab5136e5de509c3026bb49129bb67de70a90e11
Ramiro <rtorrese@proton.me>
2025-08-01 06:16:44 +0800

    minor update to mini-ai's configuration

--- nvim/.config/nvim/lua/plugins/enhancements/mini-ai.lua
+++ nvim/.config/nvim/lua/plugins/enhancements/mini-ai.lua
@@ -1,12 +1,11 @@
 return {
 	"echasnovski/mini.ai",
 	version = false,
-	event = "BufReadPost",
+	event = "VeryLazy",
 	opts = function()
 		local ai = require("mini.ai")
 		return {
 			n_lines = 500,
-			search_method = "cover_or_next",
 			custom_textobjects = {
 				o = ai.gen_spec.treesitter({ -- code block
 					a = { "@block.outer", "@conditional.outer", "@loop.outer" },
@@ -15,12 +14,13 @@ return {
 				f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
 				c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
 				t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+				d = { "%f[%d]%d+" }, -- digits
 				e = { -- Word with case
 					{ "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
 					"^().*()$",
 				},
-				u = ai.gen_spec.function_call(), -- u for usage
-				U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }),
+				u = ai.gen_spec.function_call(), -- u for "Usage"
+				U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
 			},
 			mappings = {
 				-- Main textobject prefixes
