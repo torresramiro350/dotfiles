9f19716a144cb7d8af676fadfed7e7d0990dd59e
Ramiro <rtorrese@proton.me>
2025-08-02 03:44:21 +0800

    extending the functionality of the flash plugin
    this allows to show the diagnostics of a target without moving the
    cursor from the current position

--- nvim/.config/nvim/lua/plugins/enhancements/flash.lua
+++ nvim/.config/nvim/lua/plugins/enhancements/flash.lua
@@ -40,6 +40,22 @@ return {
 		},
 	},
 	opts = {
+		-- show diagnostics at target without changing the cursor position
+		matcher = function(win)
+			return vim.tbl_map(function(diag)
+				return {
+					pos = { diag.lnum + 1, diag.col },
+					end_pos = { diag.end_lnum + 1, diag.end_col - 1 },
+				}
+			end, vim.diagnostic.get(vim.api.nvim_win_get_buf(win)))
+		end,
+		action = function(match, state)
+			vim.api.nvim_win_call(match.win, function()
+				vim.api.nvim_win_set_cursor(match.win, match.pos)
+				vim.diagnostic.open_float()
+			end)
+			state:restore()
+		end,
 		modes = {
 			char = {
 				jump_labels = true,
