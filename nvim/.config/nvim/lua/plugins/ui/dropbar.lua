c6ce56ea5a62185aa7b773ea39060b24e71351c9
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-08-02 10:39:21 +0800

    not fully convinced in keeping this plugin

--- nvim/.config/nvim/lua/plugins/ui/dropbar.lua
+++ nvim/.config/nvim/lua/plugins/ui/dropbar.lua
@@ -1,6 +1,7 @@
 return {
 	"Bekaboo/dropbar.nvim",
 	event = { "BufRead", "BufReadPre", "BufNewFile" },
+  enabled = false,
 	opts = {},
 	config = function(_, opts)
 		local dropbar_api = require("dropbar.api")
