ac1f40995b142b4b9204b043c3e8d0b5919fe8f1
Ramiro <rtorrese@proton.me>
2025-08-04 20:49:00 +0800

    minor change to the minitabline config file

--- nvim/.config/nvim/lua/plugins/ui/mini-tabline.lua
+++ nvim/.config/nvim/lua/plugins/ui/mini-tabline.lua
@@ -1,15 +1,15 @@
 return {
-  "echasnovski/mini.tabline",
-  version = false,
-  enabled = true,
-  -- version = "*",
-  event = { "BufRead", "BufReadPre", "BufNewFile" },
-  config = function()
-    local tabline = require("mini.tabline")
-    tabline.setup({
-      show_icons = true,
-      set_vim_settings = true,
-      tabpage_section = "left",
-    })
-  end,
+	"echasnovski/mini.tabline",
+	version = false,
+	enabled = false,
+	version = "*",
+	event = { "BufRead", "BufReadPre", "BufNewFile" },
+	config = function()
+		local tabline = require("mini.tabline")
+		tabline.setup({
+			show_icons = true,
+			set_vim_settings = true,
+			tabpage_section = "left",
+		})
+	end,
 }
