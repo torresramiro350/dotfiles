e0eccb4cad33e7ddd88038ed1f4db6d24256e65c
Ramiro <rtorrese@proton.me>
2025-08-04 20:49:14 +0800

    bringing bufferline back

---
+++ nvim/.config/nvim/lua/plugins/ui/bufferline.lua
@@ -0,0 +1,38 @@
+return {
+	"akinsho/bufferline.nvim",
+	-- event = "VeryLazy",
+	event = { "BufEnter", "BufReadPre" },
+	opts = {
+		options = {
+			close_command = function(n)
+				Snacks.bufdelete(n)
+			end,
+			right_mouse_command = function(n)
+				Snacks.bufdelete(n)
+			end,
+			always_show_bufferline = false,
+			diagnostics = "nvim_lsp",
+			offsets = {
+				{
+					filetype = "neo-tree",
+					text = "Neo-tree",
+					highlight = "Directory",
+					text_align = "left",
+				},
+				{
+					filetype = "snacks_layout_box",
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		require("bufferline").setup(opts)
+		vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+			callback = function()
+				vim.schedule(function()
+					pcall(nvim_bufferline)
+				end)
+			end,
+		})
+	end,
+}
