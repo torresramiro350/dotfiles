e4aef704a94adac67daf80aec16402c1ac730f43
Ramiro <rtorrese@proton.me>
2025-08-06 03:00:50 +0800

    cleaning out several plugins I don't use

--- nvim/.config/nvim/lua/plugins/ui/dropbar.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"Bekaboo/dropbar.nvim",
-	event = { "BufRead", "BufReadPre", "BufNewFile" },
-  enabled = false,
-	opts = {},
-	config = function(_, opts)
-		local dropbar_api = require("dropbar.api")
-	end,
-}
