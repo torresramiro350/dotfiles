41f00c978ccfeea9fa44d035a1e959e1ef76a017
Ramiro <rtorrese@proton.me>
2025-08-07 18:09:34 +0800

    integrating catppuccin with the bufferline plugin

--- nvim/.config/nvim/lua/plugins/ui/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/ui/bufferline.lua
@@ -1,34 +1,37 @@
 return {
 	"akinsho/bufferline.nvim",
-	-- event = "VeryLazy",
 	event = { "BufEnter", "BufReadPre" },
-	opts = {
-		options = {
-			close_command = function(n)
-				Snacks.bufdelete(n)
-			end,
-			right_mouse_command = function(n)
-				Snacks.bufdelete(n)
-			end,
-			always_show_bufferline = false,
-			diagnostics = "nvim_lsp",
-			diagnostics_indicator = function(count, level, _)
-				local icon = level:match("error") and " " or " "
-				return " " .. icon .. count
-			end,
-			offsets = {
-				{
-					filetype = "neo-tree",
-					text = "Neo-tree",
-					highlight = "Directory",
-					text_align = "left",
-				},
-				{
-					filetype = "snacks_layout_box",
+	opts = function()
+		local highlights = require("catppuccin.groups.integrations.bufferline")
+		return {
+			highlights = highlights.get(),
+			options = {
+				close_command = function(n)
+					Snacks.bufdelete(n)
+				end,
+				right_mouse_command = function(n)
+					Snacks.bufdelete(n)
+				end,
+				always_show_bufferline = false,
+				diagnostics = "nvim_lsp",
+				diagnostics_indicator = function(count, level, _)
+					local icon = level:match("error") and " " or " "
+					return " " .. icon .. count
+				end,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "Neo-tree",
+						highlight = "Directory",
+						text_align = "left",
+					},
+					{
+						filetype = "snacks_layout_box",
+					},
 				},
 			},
-		},
-	},
+		}
+	end,
 	config = function(_, opts)
 		require("bufferline").setup(opts)
 		vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
