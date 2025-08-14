daca2442aa26629516e0bd32de57e3f74403e9ab
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-08-14 11:32:43 +0800

    adding more configuration to mini-diff

--- nvim/.config/nvim/lua/plugins/git/mini-diff.lua
+++ nvim/.config/nvim/lua/plugins/git/mini-diff.lua
@@ -1,6 +1,10 @@
 return {
 	"echasnovski/mini.diff",
 	event = "VeryLazy",
+	-- disabling for now, as it doesn't offer the same level of functionality as
+	-- gitsigns. Might delete later.
+	enabled = false,
+	cond = in_git(),
 	keys = {
 		{
 			"<leader>go",
@@ -10,14 +14,46 @@ return {
 			desc = "Toggle mini.diff overlay",
 		},
 	},
-	opts = {
-		view = {
-			style = "sign",
-			signs = {
-				add = "▎",
-				change = "▎",
-				delete = "",
+	opts = function()
+		-- NOTE: taken from LazyVim's config
+		Snacks.toggle({
+			name = "Mini Diff Signs",
+			get = function()
+				return vim.g.minidiff_disable ~= true
+			end,
+			set = function(state)
+				vim.g.minidiff_disable = not state
+				if state then
+					require("mini.diff").enable(0)
+				else
+					require("mini.diff").disable(0)
+				end
+				-- HACK: redraw to update the signs
+				vim.defer_fn(function()
+					vim.cmd([[redraw!]])
+				end, 200)
+			end,
+		}):map("<leader>uG")
+		return {
+			mappings = {
+				reset = "<leader>ghr",
+				apply = "<leader>ghs",
+				goto_first = "[H",
+				goto_prev = "[h",
+				goto_next = "]h",
+				goto_last = "]H",
 			},
-		},
-	},
+			view = {
+				style = "sign",
+				signs = {
+					add = "▎",
+					change = "▎",
+					delete = "",
+				},
+			},
+		}
+	end,
+	config = function(_, opts)
+		require("mini.diff").setup(opts)
+	end,
 }
