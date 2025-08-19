c466e745d1ba929cab116eaabe0d4fa90e795fe1
Ramiro <rtorrese@proton.me>
2025-08-19 22:39:04 +0800

    moving back to the shfmt formatter for bash/shell scripts

--- nvim/.config/nvim/lua/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -41,7 +41,7 @@ return {
 				fish = { "fish_indent" },
 				cmake = { "cmake_format", "gersemi" },
 				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
-				sh = { "shellharden", "beautysh" },
+				sh = { "shellharden", "shfmt" },
 				toml = { "taplo" },
 				tex = { "latexindent" },
 				typst = { "typstyle", "prettypst" },
