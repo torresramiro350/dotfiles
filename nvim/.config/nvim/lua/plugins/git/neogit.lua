12a5d90ae3b84e640b4d214baeea34f7649acc59
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-09-01 10:04:42 +0800

    removing fzf lua plugin

--- nvim/.config/nvim/lua/plugins/git/neogit.lua
+++ nvim/.config/nvim/lua/plugins/git/neogit.lua
@@ -4,7 +4,7 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- required
 		"sindrets/diffview.nvim", -- optional - Diff integration
-		"ibhagwan/fzf-lua", -- optional
+		"folke/snacks.nvim", -- optional, snacks picker
 	},
 	opts = {},
 	config = function(_, opts)
