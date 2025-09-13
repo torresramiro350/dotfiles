501ee831d86135d46f87f04b4c284852ee3ed838
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-09-13 20:34:12 +0800

    fixing a minor type in the configuration for blink

--- nvim/.config/nvim/lua/plugins/completion/blink_cmp.lua
+++ nvim/.config/nvim/lua/plugins/completion/blink_cmp.lua
@@ -59,8 +59,7 @@ return {
 							text = function(ctx)
 								local icon = ctx.kind_icon
 								if vim.tbl_contains({ "Path" }, ctx.source_name) then
-									local dev_icon, _
-									require("nvim-web-devicons").get_icon(ctx.label)
+									local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
 									if dev_icon then
 										icon = dev_icon
 									end
