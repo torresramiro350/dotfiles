ef6be74c325b11429a6a1b8d23ed6f18135d96ca
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-10-07 08:16:08 +0800

    updating some configurations

--- nvim/.config/nvim/lsp/dockerls.lua
+++ nvim/.config/nvim/lsp/dockerls.lua
@@ -1,7 +1,17 @@
 local Lsp = require("utils.lsp")
 return {
-	cmd = { "docker-langserver", "--stdio" },
+	cmd = { "docker-language-server", "start", "--stdio" },
 	on_attach = Lsp.on_attach,
-	filetypes = { "dockerfile" },
-	root_markers = { "Dockerfile" },
+	filetypes = { "dockerfile", "yaml.docker-compose" },
+	root_markers = {
+		"Dockerfile",
+		"docker-compose.yaml",
+		"docker-compose.yml",
+		"compose.yaml",
+		"compose.yml",
+		"docker-bake.json",
+		"docker-bake.hcl",
+		"docker-bake.override.json",
+		"docker-bake.override.hcl",
+	},
 }
