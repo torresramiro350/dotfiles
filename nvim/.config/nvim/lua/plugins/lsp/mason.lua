0d821748ec32a12ef27a24b94e4b33abec0de3cb
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-11-01 07:19:53 +0900

    minor cleanup

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -1,70 +1,71 @@
 return {
 	{
-		"williamboman/mason.nvim",
-		cmd = "Mason",
+		"williamboman/mason-lspconfig.nvim",
 		event = "VeryLazy",
-		opts_extended = {
-			"ensure_installed",
-		},
 		opts = {
 			ensure_installed = {
-				-- lua
 				"stylua",
-				"lua-language-server",
-				-- c/cpp
+				"lua_ls",
 				"clangd",
-				"cpplint",
-				-- bash
-				"bash-language-server",
-				"shfmt",
-				"shellharden",
-				"shellcheck",
-				"beautysh",
-				-- docker
-				"docker-language-server",
-				-- cmake
-				"cmake-language-server",
-				"cmakelint",
-				"gersemi",
-				-- markdown
-				"markdownlint-cli2",
-				"markdown-toc",
-				"markdown-oxide",
-				-- toml
+				"bashls",
+				"dockerls",
+				"cmake",
+				"marksman",
 				"taplo",
-				"pyproject-fmt",
-				-- yaml
-				"yaml-language-server",
-				"yamllint",
-				"yamlfmt",
-				-- json
-				"json-lsp",
-				"prettier",
-				"prettierd",
+				"yamlls",
+				"jsonls",
 			},
 		},
-		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-		build = ":MasonUpdate",
-		config = function(_, opts)
-			require("mason").setup(opts)
-			local mr = require("mason-registry")
-			mr:on("package:install:success", function()
-				vim.defer_fn(function()
-					-- trigger FileType event to possibly load this newly installed LSP server
-					require("lazy.core.handler.event").trigger({
-						event = "FileType",
-						buf = vim.api.nvim_get_current_buf(),
-					})
-				end, 100)
-			end)
-			mr.refresh(function()
-				for _, tool in ipairs(opts.ensure_installed) do
-					local p = mr.get_package(tool)
-					if not p:is_installed() then
-						p:install()
-					end
-				end
-			end)
-		end,
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				cmd = "Mason",
+				opts_extended = {
+					"ensure_installed",
+				},
+				opts = {
+					ensure_installed = {
+						"cpplint",
+						"cmakelint",
+						"gersemi",
+						"markdownlint-cli2",
+						"markdown-toc",
+						"prettier",
+						"prettierd",
+						"pyproject-fmt",
+						"shfmt",
+						"shellharden",
+						"shellcheck",
+						"yamllint",
+						"yamlfmt",
+					},
+				},
+				keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+				build = ":MasonUpdate",
+				config = function(_, opts)
+					require("mason").setup(opts)
+					local mr = require("mason-registry")
+					mr:on("package:install:success", function()
+						vim.defer_fn(function()
+							-- trigger FileType event to possibly load this newly installed LSP server
+							require("lazy.core.handler.event").trigger({
+								event = "FileType",
+								buf = vim.api.nvim_get_current_buf(),
+							})
+						end, 100)
+					end)
+					mr.refresh(function()
+						for _, tool in ipairs(opts.ensure_installed) do
+							local p = mr.get_package(tool)
+							if not p:is_installed() then
+								p:install()
+							end
+						end
+					end)
+				end,
+			},
+			-- load mason before neovim lsp config
+			{ "neovim/nvim-lspconfig" },
+		},
 	},
 }
