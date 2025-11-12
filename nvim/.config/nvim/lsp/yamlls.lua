e1a92e0ed4eeb0022a37d91e9ae5902dc62e9f99
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-11-12 12:44:08 +0800

    scaling down on plugins

--- nvim/.config/nvim/lsp/yamlls.lua
+++ nvim/.config/nvim/lsp/yamlls.lua
@@ -11,10 +11,10 @@ return {
 			},
 		},
 	},
-	on_new_config = function(new_config)
-		new_config.settings.yaml.schemas =
-			vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
-	end,
+	-- on_new_config = function(new_config)
+	-- 	new_config.settings.yaml.schemas =
+	-- 		vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
+	-- end,
 	settings = {
 		redhat = { telemetry = { enabled = false } },
 		yaml = {
