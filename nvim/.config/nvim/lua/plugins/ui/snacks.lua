8d0f76048f027db5c82b619e9def760f7e89b2c8
Ramiro Torres-Escobedo <rtorrese@proton.me>
2025-12-11 20:32:31 +0800

    minor formatting change

--- nvim/.config/nvim/lua/plugins/ui/snacks.lua
+++ nvim/.config/nvim/lua/plugins/ui/snacks.lua
@@ -130,7 +130,7 @@ return {
     { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
     -- LSP
     { "gd",         function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
-    { "gr",         function() Snacks.picker.lsp_references() end, nowait = true,                        desc = "References" },
+    { "gr",         function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
     { "gI",         function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
     { "gy",         function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
     { "gai", function() Snacks.picker.lsp_incoming_calls() end, desc = "C[a]lls Incoming" },
