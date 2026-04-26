f0e81e808126dbfd1b7e88c468c3c7a64d3c4236
Ramiro Torres-Escobedo <rtorrese@proton.me>
2026-04-26 22:19:56 +0800

    just applying formatting

--- nvim/.config/nvim/lua/plugins/enhancements/flash.lua
+++ nvim/.config/nvim/lua/plugins/enhancements/flash.lua
@@ -66,27 +66,31 @@ return {
     { "r",     mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R",     mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-    {"<leader>cD", mode={'n'} , function ()
-      require('flash').jump({
-        matcher=function (win)
-          return vim.tbl_map(
-            function (diag)
-              return {
-                pos = { diag.lnum + 1, diag.col },
-                end_pos = { diag.end_lnum + 1, diag.end_col - 1 },
-              }
-            end, vim.diagnostic.get(vim.api.nvim_win_get_buf(win)))
-        end,
-        action = function (match, state)
-          vim.api.nvim_win_call(match.win, function ()
-            vim.api.nvim_win_set_cursor(match.win, match.pos)
-            vim.diagnostic.open_float()
-          end)
-          state:restore()
-        end,
-      })
-    end},
 		-- stylua: ignore end
+		{
+			"<leader>cD",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump({
+					matcher = function(win)
+						return vim.tbl_map(function(diag)
+							return {
+								pos = { diag.lnum + 1, diag.col },
+								end_pos = { diag.end_lnum + 1, diag.end_col - 1 },
+							}
+						end, vim.diagnostic.get(vim.api.nvim_win_get_buf(win)))
+					end,
+					action = function(match, state)
+						vim.api.nvim_win_call(match.win, function()
+							vim.api.nvim_win_set_cursor(match.win, match.pos)
+							vim.diagnostic.open_float()
+						end)
+						state:restore()
+					end,
+				})
+			end,
+			desc = "Show diagnostics at target",
+		},
 		{
 			"<c-space>",
 			mode = { "n", "o", "x" },
