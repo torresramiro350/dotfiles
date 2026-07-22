4b74df4bbbea1a7cef596d69534c12f9f1e286c4
Ramiro Torres-Escobedo <rtorrese@proton.me>
2026-07-21 19:23:02 -0500

    give the starting screen more swag

--- nvim/.config/nvim/lua/plugins/ui/snacks.lua
+++ nvim/.config/nvim/lua/plugins/ui/snacks.lua
@@ -29,10 +29,27 @@ return {
 		dashboard = {
 			sections = {
 				{ section = "header" },
-				{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{ section = "keys", icon = " ", title = "keymaps" },
+				{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{
+					pane = 2,
+					icon = " ",
+					title = "Git Status",
+					section = "terminal",
+					enabled = function()
+						return Snacks.git.get_root() ~= nil
+					end,
+					cmd = "git status --short --branch --renames",
+					height = 5,
+					padding = 1,
+					ttl = 5 * 60,
+					indent = 3,
+				},
 				{ section = "startup" },
+				-- { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+				-- { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				-- { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
 			},
 		},
 		bigfile = { enabled = true },
