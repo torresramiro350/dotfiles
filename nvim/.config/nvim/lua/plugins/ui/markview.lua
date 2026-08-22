e49bbf8d774ee6e891dab32bc8001b9cd01de146
Ramiro Torres-Escobedo <rtorrese@proton.me>
2026-08-22 11:10:47 -0500

    improve rendering for markdown files

--- nvim/.config/nvim/lua/plugins/ui/markview.lua
+++ nvim/.config/nvim/lua/plugins/ui/markview.lua
@@ -5,7 +5,6 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"saghen/blink.cmp",
 	},
-	ft = { "markdown", "norg", "rmd", "org", "codecompanion", "blink-cmp-documentation", "blink-cmp-signature" },
 	lazy = false,
 	priority = 49,
 	enabled = true,
@@ -20,24 +19,62 @@ return {
 			ignore_buftypes = { "nofile" },
 			icon_provider = "mini",
 			debounce = 50,
+			linewise_hybrid_mode = false,
+			hybrid_modes = { "n", "i" },
+			-- modes = { "n", "no", "c" },
+			max_buf_lines = 500,
 		},
 		markdown = {
 			enable = true,
 			headings = { enable = true, shift_width = 1 },
-			code_blocks = { enable = true, style = "language", min_width = 60, pad_amount = 2 },
+			code_blocks = {
+				enable = true,
+				border_hl = "MarkviewCode",
+				info_hl = "MarkviewCodeInfo",
+				label_direction = "right",
+				label_hl = nil,
+				min_width = 100,
+				pad_amount = 2,
+				pad_char = " ",
+				default = { block_hl = "MarkviewCode", pad_hl = "MarkviewCode" },
+				["diff"] = {
+					block_hl = function(_, line)
+						if line:match("^%+") then
+							return "MarkviewPalette4"
+						elseif line:match("^%-") then
+							return "MarkviewPalette1"
+						else
+							return "MarkviewCode"
+						end
+					end,
+					pad_hl = "MarkviewCode",
+				},
+				sign = true,
+				-- style = "block",
+				style = function(buf)
+					if vim.o.wrap then
+						return "simple"
+					end
+					local win = require("markview.utils").buf_getwin(buf)
+					return vim.wo[win].wrap == true and "simple" or "block"
+				end,
+			},
 			block_quotes = { enable = true, default = { border = "▋" } },
+			list_items = { enable = true, indent_size = 2, shift_width = 2, marker_minus = { text = "" } },
+			tables = { enable = true, use_virt_lines = false, style = "rounded" },
 		},
-		list_items = { enable = true, marker_minus = { text = "•" } },
-		checkboxes = {
-			enable = true,
-			checked = { text = "✔" },
-			unchecked = { text = "✘" },
-		},
-		tables = { enable = true, style = "rounded" },
 		markdown_inline = {
 			enable = true,
+			internal_links = { enable = true },
+			embed_files = { enable = true },
+			hyperlinks = { enable = true },
+			footnotes = { enable = true },
 			inline_codes = { enable = true },
-			link = { enable = true },
+			checkboxes = {
+				enable = true,
+				checked = { text = "✔" },
+				unchecked = { text = "✘" },
+			},
 		},
 		html = { enable = true },
 		latex = { enable = true },
